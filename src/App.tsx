48c1e38488dfc37624cedc9e435cd44636737702
ECWireless <Elliott@valtechcreative.com>
2021-01-24 17:59:11 -0700

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
+import { media } from './components/Breakpoints'
+import { colors } from './components/theme'
 
 function App() {
-  return (
-    <div>
-      Plating Game
-    </div>
-  );
+	return (
+		<StyledGameContainer>
+			<StyledPlatingCanvas>
+				Plating Canvas
+			</StyledPlatingCanvas>
+			<StyledSidebar>
+				Sidebar
+			</StyledSidebar>
+			<StyledSelectionBox>
+				Selection Box
+			</StyledSelectionBox>
+		</StyledGameContainer>
+	);
 }
 
 export default App;
+
+const StyledGameContainer = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 40rem;
+	grid-template-rows: 60rem 30rem;
+
+	${media.xl`
+		grid-template-columns: 1fr 50rem;
+		grid-template-rows: 80rem 40rem;
+	`}
+`
+
+const StyledPlatingCanvas = styled.div`
+	border: 2px solid ${colors.blue};
+	grid-column: 1 / 2;
+	grid-row: 1 / 2;
+`
+
+const StyledSelectionBox = styled.div`
+	border: 2px solid ${colors.blue};
+	grid-column: 1 / 3;
+	grid-row: 2 / 3;
+`
+
+const StyledSidebar = styled.div`
+	border: 2px solid ${colors.blue};
+	grid-column: 2 / 2;
+	grid-row: 1 / 2;
+`
---
+++ src/components/theme.ts
@@ -0,0 +1,14 @@
+export const colors = {
+    black: '#000',
+    blue: '#438DD5',
+    green: '#34A548',
+    red: '#E90505',
+    white: '#FFF',
+}
+
+// export const shadows = {
+//     button: '0px 1px 2px 0px rgba(0,0,0,0.7)',
+//     buttonHover: '0px 8px 20px rgba(41, 47, 51, 0.3)',
+//     card: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+//     sidebar: '5px 3px 25px -3px rgba(0,0,0,.5)',
+// }
